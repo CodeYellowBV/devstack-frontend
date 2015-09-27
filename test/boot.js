e50081d9c2c975cf7668f7454d939808b627db1a
Kees Kluskens <kees@webduck.nl>
2015-09-27 16:51:10 +0200

    Use jsdom for testing.

--- test/boot.js
+++ test/boot.js
@@ -1,17 +1,29 @@
 import 'init';
 import './spec/index';
 import sinon from 'sinon';
-// import loca from 'loca';
+import loca from 'loca';
+
+let failedCount = 0;
 
 // Add a reporter that logs everying to the console.
 // TODO: When using this, the web reporter is disabled. Multi-reporter support has not landed yet.
-// Maybe we should write our own hack for this? We need a hack anyway to forward the correct exit code to run-chromium-headless
-// mocha.reporter(loca);
+mocha.reporter(loca);
+
+mocha.suite.afterAll(function(done) {
+    if (_.isFunction(window.onMochaTestFinish)) {
+        window.onMochaTestFinish(failedCount);
+    }
+    done();
+});
 
 beforeEach(function() {
     this.sinon = sinon.sandbox.create();
 });
 
 afterEach(function() {
+    if (this.currentTest.state === 'failed') {
+        failedCount++;
+    }
+
     this.sinon.restore();
 });
